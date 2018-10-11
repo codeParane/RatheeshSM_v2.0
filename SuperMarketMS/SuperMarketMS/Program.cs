5b1b15bba7edeaeaec89162188bfb206e389869a
Paranetharan Jeevaraj <32712199+codeparane@users.noreply.github.com>
2018-10-11 21:20:05 +0530

    some changes

--- SuperMarketMS/SuperMarketMS/Program.cs
+++ SuperMarketMS/SuperMarketMS/Program.cs
@@ -16,7 +16,7 @@ namespace SuperMarketMS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Print());
+            Application.Run(new SalesRegister());
         }
     }
 }
